043dc6a24c8ffcfed1a3c8cf78953fc00b4fed4c
Zhao Ziloong <ziloon.z@icloud.com>
2024-04-04 00:53:23 +0800

    尝试各种测试用例写法

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,38 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
+    fn new(width: u32, height: u32) -> Rectangle {
+        Rectangle { width, height }
+    }
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -10,5 +42,46 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn exploration() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four."))
+        }
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle::new(8, 7);
+        let smaller = Rectangle::new(5, 7);
+        assert!(larger.can_hold(&smaller));
+        assert_eq!(smaller.can_hold(&larger), false);
+    }
+
+    #[test]
+    fn larger_can_hold_same() {
+        let larger = Rectangle::new(8, 7);
+        let smaller = Rectangle::new(8, 7);
+        assert!(larger.can_hold(&smaller));
+        assert!(smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be between 1 and 100")]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
